634242e7b931912315320b70914e4bf2a0bf665d
Серова Дарья Викторовна <dserova@apkholding.ru>
2023-08-03 22:06:00 +0700

    sprint 14

--- src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
@@ -10,31 +10,31 @@ import java.util.List;
 import java.util.Optional;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
-    Optional<List<Booking>> findByBooker_IdOrderByStartDesc(long booker_id);
+    Optional<List<Booking>> findByBooker_IdOrderByStartDesc(long bookerId);
 
-    Optional<List<Booking>> findByBooker_IdAndStartAfterOrderByStartDesc(long booker_id, @NonNull LocalDateTime start);
+    Optional<List<Booking>> findByBooker_IdAndStartAfterOrderByStartDesc(long bookerId, @NonNull LocalDateTime start);
 
-    Optional<List<Booking>> findByBooker_IdAndStartBeforeAndEndAfterOrderByStartDesc(long booker_id, @NonNull LocalDateTime start, @NonNull LocalDateTime end);
+    Optional<List<Booking>> findByBooker_IdAndStartBeforeAndEndAfterOrderByStartDesc(long bookerId, @NonNull LocalDateTime start, @NonNull LocalDateTime end);
 
-    Optional<List<Booking>> findByBooker_IdAndEndBeforeOrderByStartDesc(long booker_id, @NonNull LocalDateTime end);
+    Optional<List<Booking>> findByBooker_IdAndEndBeforeOrderByStartDesc(long bookerId, @NonNull LocalDateTime end);
 
-    Optional<List<Booking>> findByBooker_IdAndStatusOrderByStartDesc(long booker_id, Status status);
+    Optional<List<Booking>> findByBooker_IdAndStatusOrderByStartDesc(long bookerId, Status status);
 
-    Optional<List<Booking>> findByItem_Owner_IdOrderByStartDesc(long item_owner_id);
+    Optional<List<Booking>> findByItem_Owner_IdOrderByStartDesc(long itemOwnerId);
 
-    Optional<List<Booking>> findByItem_Owner_IdAndStartAfterOrderByStartDesc(long item_owner_id, @NonNull LocalDateTime start);
+    Optional<List<Booking>> findByItem_Owner_IdAndStartAfterOrderByStartDesc(long itemOwnerId, @NonNull LocalDateTime start);
 
-    Optional<List<Booking>> findByItem_Owner_IdAndStartBeforeAndEndAfterOrderByStartDesc(long item_owner_id, @NonNull LocalDateTime start, @NonNull LocalDateTime end);
+    Optional<List<Booking>> findByItem_Owner_IdAndStartBeforeAndEndAfterOrderByStartDesc(long itemOwnerId, @NonNull LocalDateTime start, @NonNull LocalDateTime end);
 
-    Optional<List<Booking>> findByItem_Owner_IdAndEndBeforeOrderByStartDesc(long item_owner_id, @NonNull LocalDateTime end);
+    Optional<List<Booking>> findByItem_Owner_IdAndEndBeforeOrderByStartDesc(long itemOwnerId, @NonNull LocalDateTime end);
 
-    Optional<List<Booking>> findByItem_Owner_IdAndStatusOrderByStartDesc(long item_owner_id, Status status);
+    Optional<List<Booking>> findByItem_Owner_IdAndStatusOrderByStartDesc(long itemOwnerId, Status status);
 
-    Optional<Booking> findFirstByBooker_IdAndEndBeforeOrderByStartDesc(long booker_id, @NonNull LocalDateTime end);
+    Optional<Booking> findFirstByBooker_IdAndEndBeforeOrderByStartDesc(long bookerId, @NonNull LocalDateTime end);
 
-    Optional<Booking> findFirstByItem_IdAndItem_Owner_IdAndStartAfterAndStatusOrderByStartAsc(long item_id, long item_owner_id, @NonNull LocalDateTime start, Status status);
+    Optional<Booking> findFirstByItem_IdAndItem_Owner_IdAndStartAfterAndStatusOrderByStartAsc(long itemId, long itemOwnerId, @NonNull LocalDateTime start, Status status);
 
-    Optional<Booking> findFirstByItem_IdAndItem_Owner_IdAndStartBeforeAndStatusOrderByStartDesc(long item_id, long item_owner_id, @NonNull LocalDateTime end, Status status);
+    Optional<Booking> findFirstByItem_IdAndItem_Owner_IdAndStartBeforeAndStatusOrderByStartDesc(long itemId, long itemOwnerId, @NonNull LocalDateTime end, Status status);
 
-    Optional<Booking> findByIdAndBooker_IdOrIdAndItem_Owner_Id(long id, long booker_id, long id2, long item_owner_id);
+    Optional<Booking> findByIdAndBooker_IdOrIdAndItem_Owner_Id(long id, long bookerId, long id2, long itemOwnerId);
 }
--- src/main/java/ru/practicum/shareit/comment/repository/CommentRepository.java
+++ src/main/java/ru/practicum/shareit/comment/repository/CommentRepository.java
@@ -7,5 +7,5 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    Optional<List<Comment>> findByItem_Id(long item_id);
+    Optional<List<Comment>> findByItem_Id(long itemId);
 }
